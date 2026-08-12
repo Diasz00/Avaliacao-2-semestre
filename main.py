d5b52d9b9b7ec93901f0a7f0490af352018faadf
Diasz00 <0080709@sesi-es.org.br>
2026-08-11 21:00:08 -0300

    Criando menu principal

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#criqndo
+import csv
+import os
+
+# Menu Principal
+while True:
+    print("\n===== MENU BIBLIOTECA =====")
+    print("1 - Cadastrar livro")
+    print("2 - Listar livros")
+    print("3 - Buscar livro")
+    print("4 - Empréstimo")
+    print("5 - Devolução")
+    print("6 - Ordenar lista")
+    print("7 - Sair")
+
+    opcao = input("Escolha uma opção: ").strip()
+
+    if opcao == "1":
+        print("Cadastrando livro...")
+    
